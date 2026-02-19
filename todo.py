c6fabc23d5c1ffb57215d4219bd379f6e470f7b0
dualio26 <eamongreen59@gmail.com>
2026-02-19 23:47:54 +1300

    Fix TodoList indenting

--- todo.py
+++ todo.py
@@ -17,38 +17,38 @@ class TodoItem:
             item.completed = data.get("completed")
             return item
 
-    class TodoList:
-        def __init__(self):
-            self.active = []
-            self.archived = []
-            self.next_id = 1
+class TodoList:
+    def __init__(self):
+        self.active = []
+        self.archived = []
+        self.next_id = 1
         
-        def add(self, text: str) -> TodoItem:
-            item = TodoItem(self.next_id, text)
-            self.next_id += 1
-            self.active.append(item)
-            return item
+    def add(self, text: str) -> TodoItem:
+        item = TodoItem(self.next_id, text)
+        self.next_id += 1
+        self.active.append(item)
+        return item
 
-        def completed(self, id:int) -> bool:
-            for item in self.active:
-                if item.id == id:
-                    item.completed = datetime.now().isoformat()
-                    self.archived.append(item)
-                    self.active.remove(item)
-                    return True
-            return False
+    def completed(self, id:int) -> bool:
+        for item in self.active:
+            if item.id == id:
+                item.completed = datetime.now().isoformat()
+                self.archived.append(item)
+                self.active.remove(item)
+                return True
+        return False
 
-        def delete(self, id: int) -> bool:
-            for item in self.active:
-                if item.id == id:
-                    self.active.remove(item)
-                    return True
-            return False
+    def delete(self, id: int) -> bool:
+        for item in self.active:
+            if item.id == id:
+                self.active.remove(item)
+                return True
+        return False
 
-        def list_active(self):
-            return self.active
+    def list_active(self):
+        return self.active
     
-        def list_archived(self):
-            return self.archived
+    def list_archived(self):
+        return self.archived
 
 pass
